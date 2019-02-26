1b7881a07af002ea9b3b74675389a0896d15ff45
bandlamudi1981 <jagadeesh.bandlamudi@gmail.com>
2019-02-25 17:42:33 -0800

    Initial Commit

---
+++ FavMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavMovies
+//
+//  Created by Srivalli Kasavajhula on 2/25/19.
+//  Copyright © 2019 Jagadeesh Bandlamudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
